41506f03359fc487e1101294de0f6a79fabdf02f
K Bhaskar Rao <kbhaskarrao022@gmail.com>
2026-01-07 13:43:16 +0530

    Updaet UI

--- client/src/features/reviews/ReviewPage.tsx
+++ client/src/features/reviews/ReviewPage.tsx
@@ -106,10 +106,9 @@ export default function ReviewPage() {
                 </div>
 
                 {/* Scrollable input area */}
-                <div className="fflex flex-col min-h-0 lg:flex-1">
+                <div className="flex-1 min-h-0 flex flex-col">
                     <textarea
-                        className="w-full resize-none rounded-lg lg:rounded-xl border border-slate-800 bg-slate-950
-                        p-3 text-sm text-slate-100 outline-none focus:border-sky-500 disabled:opacity-60 min-h-[260px] lg:min-h-0 lg:flex-1"
+                        className="flex-1 min-h-0 w-full resize-none rounded-lg lg:rounded-xl border border-slate-800 bg-slate-950 p-3 text-sm text-slate-100 outline-none focus:border-sky-500 disabled:opacity-60"
                         placeholder="Paste your JavaScript/React/Node.js code here..."
                         value={code}
                         onChange={(e) => setCode(e.target.value)}
@@ -122,7 +121,7 @@ export default function ReviewPage() {
                         </p>
                     )}
                 </div>
-
+                
                 {/* Controls */}
                 <div className="mt-3 flex flex-col gap-2 lg:flex-row lg:items-center lg:flex-nowrap">
 
